d595ede5aec34bebc2179e0093a96157e2e885df
vitourwd <vitorwilliamd@gmail.com>
2025-01-11 17:27:17 -0300

    testes aba de busca

--- Webmotors/tests/test_busca.py
+++ Webmotors/tests/test_busca.py
@@ -1,39 +1,78 @@
-# This sample code supports Appium Python client >=2.3.0
-# pip install Appium-Python-Client
-# Then you can paste this into a file and simply run with Python
-
+import pytest
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
-
-# For W3C actions
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.actions import interaction
-from selenium.webdriver.common.actions.action_builder import ActionBuilder
-from selenium.webdriver.common.actions.pointer_input import PointerInput
-
-options = AppiumOptions()
-options.load_capabilities({
-	"appium:automationName": "UIAutomator2",
-	"appium:platformName": "Android",
-	"appium:newCommandTimeout": 3600,
-	"appium:connectHardwareKeyboard": True,
-	"appium:appPackage": "hands.android.webmotors",  # Exemplo: Calculadora
-	"appium:appActivity": ".Webmotors",
-	"appium:udid": "RQCT302JFED",
-})
-
-driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+from selenium.common import NoSuchElementException
+import time
+import logging
 
 
-def test_busca_carro():
-	el3 = driver.find_element(by=AppiumBy.ID, value="hands.android.webmotors:id/et_search_view")
-	el3.send_keys("honda fit")
-	el4 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"Honda Fit\")")
-	el4.click()
-	el5 = driver.find_element(by=AppiumBy.ID, value="hands.android.webmotors:id/tvTitle")
-	el5.click()
 
-	assert True
+class TestBuscaWebmotors():
+
+	capabilities = {
+		"appium:automationName": "UIAutomator2",
+		"appium:platformName": "Android",
+		"appium:newCommandTimeout": 3600,
+	}
+
+	url = "http://127.0.0.1:4723"
+	options = AppiumOptions()
+	options.load_capabilities(capabilities)
+
+	driver = webdriver.Remote(url, options=options)
+	driver.implicitly_wait(10)
+
+	button_filter = "hands.android.webmotors:id/iv_filter"
+	button_filters = "hands.android.webmotors:id/btn_filter"
+	button_tab_search= "hands.android.webmotors:id/navigation_search"
+	button_accept_location_app = "hands.android.webmotors:id/btOkPermission"
+	button_accept_location_permission = "com.android.permissioncontroller:id/permission_allow_foreground_only_button"
+
+	input_search_car = "hands.android.webmotors:id/et_search_view"
 
 
+	def test_buscar_por_carro_no_campo_de_texto(self):
+
+		# Seleciona o botão 'Buscar'
+		self.driver.find_element(by=AppiumBy.ID, value=self.button_tab_search).click()
+		time.sleep(5)
+		# Digita o nome/modelo do carro no campo de busca
+		self.driver.find_element(by=AppiumBy.ID, value=self.input_search_car).send_keys("honda fit")
+		time.sleep(5)
+		# Clica no elemento de destaque de Busca
+		self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"Honda Fit\")").click()
+		time.sleep(2)
+
+		try :
+			# Verifica se aparece pop-up de localizacao
+			if self.driver.find_element(by=AppiumBy.ID, value=self.button_accept_location_app).is_displayed():
+				# Clica para permitir a localizacao
+				self.driver.find_element(by=AppiumBy.ID, value=self.button_accept_location_app).click()
+
+				# Verifica se aparece o pop-up de permissao de localizacao do android
+				if self.driver.find_element(by=AppiumBy.ID, value=self.button_accept_location_permission).is_displayed():
+					# Clica para permitir que o aplicativo utilize a localizacao
+					self.driver.find_element(by=AppiumBy.ID,value=self.button_accept_location_permission).click()
+		except NoSuchElementException as e:
+			logging.info(e)
+		# Clica no botao de filtros
+		self.driver.find_element(by=AppiumBy.ID, value=self.button_filter).click()
+		time.sleep(2)
+
+		# Valida se os valores dos campos de Marca/Modelo batem com o que foi pesquisado pelo campo de busca
+		assert self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"HONDA\")").is_displayed() and self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"FIT\")").is_displayed()
+
+	# @pytest.mark.parametrize("")
+	def test_busca_rapida(self):
+		# Seleciona o botão 'Buscar'
+		self.driver.find_element(by=AppiumBy.ID, value=self.button_tab_search).click()
+		time.sleep(2)
+		# Seleciona uma busca de marca rapida
+		self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"CHEVROLET\")").click()
+		time.sleep(5)
+		# Clica no botao de filtro
+		self.driver.find_element(by=AppiumBy.ID, value=self.button_filter).click()
+
+		# Valida se a marca selecionada nos filtros é a mesma que foi selecionada na busca rapida
+		assert self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"CHEVROLET\")").is_displayed()
